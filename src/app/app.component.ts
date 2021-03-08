9cceea2b2daf8e6395dd953cdcd111fb1b7a2f8a
Alan Camacho <leonel1672@gmail.com>
2021-03-07 22:25:11 -0600

    feat(): gallery-item controller base creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { animate, style } from '@angular/animations';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { GalleryService } from './services/gallery.service';
+import { Painting } from './models/item-gallery/item-gallery.model'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'harvard-museum';
+export class AppComponent implements OnInit{
+  galleryData: Painting[] = [];
+  pageNumber: number = 1;
+  lastScroll: boolean = false;
+  totalPages: number = null;
+
+  constructor(
+    private gs: GalleryService,
+    private spinner: NgxSpinnerService
+  ){}
+
+  ngOnInit(){}
+
+  loadGallery(){}
+
+  addToGallery(){}
+
+  loadNewPaintings(){}
+
 }
