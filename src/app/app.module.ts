828d93b5986a26cafebf83195a6eb190392539b6
Alan Camacho <leonel1672@gmail.com>
2021-03-07 21:52:35 -0600

    feat(): modules are added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { NgxSpinnerModule } from "ngx-spinner";
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { NgxMasonryModule } from 'ngx-masonry';
 
 import { AppComponent } from './app.component';
+import { GalleryItemComponent } from './components/gallery-item/gallery-item.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BrowserAnimationsModule,
+    GalleryItemComponent,
+    HttpClientModule,
+    InfiniteScrollModule,
+    NavbarComponent,
+    NgxMasonryModule,
+    NgxSpinnerModule
   ],
   imports: [
     BrowserModule
