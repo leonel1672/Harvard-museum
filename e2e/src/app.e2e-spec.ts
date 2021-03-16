4e413ef2bd53b23d022a0ae4f9ae7d6ff194f13d
Alan Leonel Camacho <acamacho@anzen.com.mx>
2021-03-16 06:26:54 -0600

    test(): e2e tests are created for AppComponent

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,5 +1,6 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { browser, logging, by, element } from 'protractor';
+
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -8,10 +9,37 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('harvard-museum app is running!');
+  it('the container element is expected to be present when the page is loaded.', () => {
+    browser.get(browser.baseUrl);
+    let container = element(by.css('app-root .container')).isPresent()
+    expect(container).toBe(true);
+   });
+
+  it('the last-scroll element is expected to be hidden', () => {
+   browser.get(browser.baseUrl);
+   let lastScroll = element(by.css('.last-scroll')).isPresent()
+   expect(lastScroll).toBe(false);
   });
+
+  it('it is expected to load multiple gallery-card-section', () => {
+    element.all(by.css('.gallery-card-section')).then( galleryElements => {
+      expect(galleryElements.length).toBeGreaterThan(0)
+    })
+  })
+
+  it('all items are expected to have a number, title and class.', () => {
+    browser.waitForAngular().then(time => {
+      if(time){
+         let cardNumber = element(by.css('.gallery-description-number')).isPresent();
+         let cardTitle = element(by.css('.gallery-description-title')).isPresent();
+         let cardClass = element(by.css('.gallery-description-classification')).isPresent();
+         expect(cardNumber).toBe(true);
+         expect(cardTitle).toBe(true);
+         expect(cardClass).toBe(true);
+         
+      }
+    })
+  })
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -6,6 +6,6 @@ export class AppPage {
   }
 
   getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+    return element(by.css('app-root .titkeExample')).getText() as Promise<string>;
   }
 }
