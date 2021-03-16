5f002a57f881199427f48f24902972c3463303bc
Alan Leonel Camacho <acamacho@anzen.com.mx>
2021-03-16 06:24:34 -0600

    chore(): autogenerated test files are deleted

--- src/app/components/overlay-gallery/overlay-gallery.component.ts
+++ src/app/components/overlay-gallery/overlay-gallery.component.ts
@@ -11,7 +11,7 @@ export class OverlayGalleryComponent implements OnInit {
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: {image: string}) { }
 
-  ngOnInit(): void {
+  ngOnInit(){
   }
 
 }
